b8afe69ac480959915c669af599d316572ce5214
Sumit <skmishracodes@gmail.com>
2024-11-01 16:36:18 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/portfolio-assignment/",
+    base: "./portfolio-assignment/",
   plugins: [react()],
 })
